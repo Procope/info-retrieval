237b083157377cc0942ec14750f8bab6b2fdb59c
akashrajkn <akashrajkn@gmail.com>
2018-01-27 13:48:57 +0100

    Statistics for PLM, Test dataset

--- HW2/generate_stats.py
+++ HW2/generate_stats.py
@@ -1,7 +1,9 @@
 import subprocess
 
 
-data_set = 'validation'
+data_set = 'test'
+score = 'relative_P_5'
+score_filename = 'precision5'
 mus = [500, 1000, 1500]
 kernel = ['passage', 'gaussian', 'triangle', 'hamming', 'circle']
 info_ret_path = '/home/akashrajkn/Documents/github_projects/'
@@ -11,14 +13,14 @@ for k in kernel:
     for mu in mus:
         print('{0} {1}'.format(k, str(mu)))
 
-        run_path = info_ret_path + 'info-retrieval/HW2/runs//plm/plm_{0}_{1}_{2}.run'.format(k, str(mu), data_set)
-        command = 'trec_eval -m all_trec -q {0} {1} | grep -E "ndcg_cut_10\s"'.format(qrel_path, run_path)
+        run_path = info_ret_path + 'info-retrieval/HW2/runs/plm/plm_{0}_{1}_{2}.run'.format(k, str(mu), data_set)
+        command = 'trec_eval -m all_trec -q {0} {1} | grep -E "{2}\s"'.format(qrel_path, run_path, score)
 
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         out, err = process.communicate()
         out = out.decode("utf-8")
 
-        filename = 'plm_{0}_{1}_ndcg10_{2}.txt'.format(k, str(mu), data_set)
+        filename = 'plm_{0}_{1}_{2}_{3}.txt'.format(k, str(mu), score_filename, data_set)
         stats_path = info_ret_path + 'info-retrieval/HW2/statistics/plm/{}/'.format(data_set) + filename
 
         with open(stats_path, 'w+') as f:
